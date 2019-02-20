3f56baf492a0f6adb4a011cba6974283be13b87b
albinjal <albin.jaldevik@gmail.com>
2019-02-20 18:23:13 +0100

    Create albja664.adb

---
+++ private/albja664.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+
+
+procedure albja664 is
+I,A:Integer;
+begin
+Get(I);
+for 1..I loop
+
+
+
+end albja664;
