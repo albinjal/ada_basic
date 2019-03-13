63df9f1d822c270e06170b99634a541112cc442d
albja664 <albja664@roxen2-117.ad.liu.se>
2019-03-13 18:14:28 +0100

    Lab 5 A Done

--- Lab5/sorted_list.ads
+++ Lab5/sorted_list.ads
@@ -1,9 +1,24 @@
 
 package Sorted_List is
-   
+   CANTFIND_ERROR: exception;
    type List_Type is private;
-   type Post is private;
-   procedure Insert();
+   
+   function Empty(List: List_Type)
+		 return Boolean;
+   function Member(List: List_Type; Search: Integer)
+		  return Boolean;
+   function Find(List: List_Type; Search: Integer)
+		return Integer;
+   function Length(List: List_Type)
+		  return Integer;
+   
+   procedure Insert(List: in out List_Type; Int: in Integer);
+   procedure Put(List: in List_Type);
+   procedure Remove(List: in out List_Type; Search: in Integer);
+   procedure Delete(List: in out List_Type);
+   procedure Find(List: in List_Type; Search: in Integer; Data: out Integer);
+   
+   
 private
    type Post;
    
